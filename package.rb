068c2206110b1e658e025e40c799cfa373adbcd0
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2013-02-05 23:38:44 -0500

    compile and update package.rb. closes #5

--- package.rb
+++ package.rb
@@ -1,7 +1,9 @@
 # Moves files from development to app. Sets up proper chrome extension
-`cp -r dev/public/* app/`
-
 # compile coffee script/sass/slim :)
-`coffee --compile --output dev/views/quick_taskk.coffee app/js/`
+`coffee --compile dev/views/quicktaskk.coffee`
+`coffee --compile dev/views/taskk.coffee`
 `sass dev/views/taskk_style.sass app/css/taskk_style.css`
 `slimrb dev/views/taskk.slim --pretty > app/taskk.html`
+
+`cp -r dev/public/* app/`
+`cp -r dev/views/*.js app/js/`
